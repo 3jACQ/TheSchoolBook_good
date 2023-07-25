d2da88c7fd781293d0472533bd61af291a70feaa
jacques <jacq.dumora@gmail.com>
2023-07-25 14:18:45 +0200

    og config

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,21 +4,61 @@ import '@/styles/globals.css'
 import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "@/components/ui/toaster"
 import { Analytics } from "@/components/analytics"
+import { siteConfig } from "@/config/site"
 export const metadata = {
-  title: 'TheschoolBook',
-  description: 'Work in progress'
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    "School",
+    "IPFS",
+    "Open Source",
+    "File Sharing",
+    "Decentralized",
+    "Library",
+    "Students",
+    "Knowledge",
+    "Sharing",
+    "Documents",
+    "Books",
+    "Notes",
+    "Summarize",
+    "Translate",
+    "Mind Map",
+    "AI",
+  ],
+  authors: [
+    {
+      name: "JacquesD",
+
+    },
+  ],
+  creator: "JacquesD",
+  openGraph: {
+    type: "website",
+    locale: "fr_FR",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+  icons: {
+    icon: "/lg.svg",
+  },
 }
 
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
     <html lang="fr" suppressHydrationWarning>
       <body>
-        
-      <ThemeProvider attribute="class">
-        {children}
-        <Toaster />
-        <Analytics/>
-      </ThemeProvider>
+
+        <ThemeProvider attribute="class">
+          {children}
+          <Toaster />
+          <Analytics />
+        </ThemeProvider>
 
       </body>
     </html>
---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/types"
+
+export const siteConfig: SiteConfig = {
+  name: "The School Book",
+  description:
+    "The open source library for the school of the future.",
+  url: "https://theschoolbook.app/",
+    ogImage: "",
+  links: {
+    twitter: "",
+    github: "https://github.com/Jacqkues/TheSchoolBook",
+  },
+}
