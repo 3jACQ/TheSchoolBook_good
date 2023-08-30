bffabebfa6e8b8c06666345dba9b595da98097e3
jacques <jacq.dumora@gmail.com>
2023-08-30 16:08:31 +0200

    try to fix :(

--- middleware.ts
+++ middleware.ts
@@ -3,45 +3,42 @@ import { withAuth } from "next-auth/middleware"
 import { NextResponse } from "next/server"
 
 export default withAuth(
-    async function middleware(req) {
-        const token = await getToken({ req })
-        const isAuth = !!token
-        const isAuthPage = req.nextUrl.pathname.startsWith("/login")
-        if (req.nextUrl.pathname.startsWith("/_next")) return NextResponse.next();
-        if (isAuthPage) {
-            if (isAuth) {
-                return NextResponse.redirect(new URL("/feed", req.url))
-            }
-            return null;
-        }
+  async function middleware(req) {
+    const token = await getToken({ req })
+    const isAuth = !!token
+    const isAuthPage = req.nextUrl.pathname.startsWith("/login") 
 
-        if (!isAuth) {
-            let from = req.nextUrl.pathname
-            if (req.nextUrl.search) {
-                from += req.nextUrl.search
-            }
-            return NextResponse.redirect(
-                new URL(`/login?from=${encodeURIComponent(from)}`, req.url)
-            )
-        }else{
-            /*return NextResponse.redirect(
-                new URL(`/wip`,req.url)
-            )*/
-        }
+    if (isAuthPage) {
+      if (isAuth) {
+        return NextResponse.redirect(new URL("/feed", req.url))
+      }
 
-        
-    },
-    {
-        callbacks: {
-            async authorized(){
-                return true;
-            },
-        },
+      return null
     }
+
+    if (!isAuth) {
+      let from = req.nextUrl.pathname;
+      if (req.nextUrl.search) {
+        from += req.nextUrl.search;
+      }
+
+      return NextResponse.redirect(
+        new URL(`/login?from=${encodeURIComponent(from)}`, req.url)
+      );
+    }
+  },
+  {
+    callbacks: {
+      async authorized() {
+        // This is a work-around for handling redirect on auth pages.
+        // We return true here so that the middleware function above
+        // is always called.
+        return true
+      },
+    },
+  }
 )
 
-
-
 export const config = {
-    matcher: ["/:path","/login"],
+  matcher: ["/:path*", "/login"],
 }
